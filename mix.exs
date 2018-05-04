e352ca0cc52d9d112dc66b9f02dba6afe7205d7c
Colin Cromar <ccromar@instructure.com>
2018-05-03 23:51:49 -0600

    adds httpoison as dependency

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ChocolateRain.Mixfile do
       app: :chocolate_rain,
       version: "0.1.0",
       elixir: "~> 1.5",
+      name: "Chocolate Rain",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -21,8 +22,8 @@ defmodule ChocolateRain.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.0"},
+      {:dogma, "~> 0.1", only: :dev}
     ]
   end
 end
